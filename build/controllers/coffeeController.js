0ea3d44d3ee46b89e46240ef9cfc3b50f7305a2e
Adam3192 <jeremyadixon@hotmail.com>
2022-08-15 14:32:07 -0500

    lesson 7 sequelize

--- build/controllers/coffeeController.js
+++ build/controllers/coffeeController.js
@@ -11,27 +11,64 @@ const allCoffee = async (req, res, next) => {
     res.render('all-coffee', { coffeeList });
 };
 exports.allCoffee = allCoffee;
-const oneCoffee = (req, res, next) => {
-    throw 'Not implemented';
+const oneCoffee = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let coffeeItem = await coffee_1.Coffee.findByPk(itemId);
+    if (coffeeItem) {
+        res.render('coffee-detail', { foundCoffee: coffeeItem });
+    }
+    else {
+        res.status(404).render('error', { message: 'coffee not found' });
+    }
 };
 exports.oneCoffee = oneCoffee;
 const addCoffeePage = (req, res, next) => {
-    throw 'Not implemented';
+    res.render('add-coffee');
 };
 exports.addCoffeePage = addCoffeePage;
-const addCoffee = (req, res, next) => {
-    throw 'Not implemented';
+const addCoffee = async (req, res, next) => {
+    let newCoffee = req.body;
+    await coffee_1.Coffee.create(newCoffee);
+    res.redirect('/coffee');
 };
 exports.addCoffee = addCoffee;
-const editCoffeePage = (req, res, next) => {
-    throw 'Not implemented';
+const editCoffeePage = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let coffeeItem = await coffee_1.Coffee.findOne({
+        where: { coffeeId: itemId }
+    });
+    if (coffeeItem) {
+        res.render('edit-coffee', { foundCoffee: coffeeItem });
+    }
+    else {
+        res.status(404).render('error', { message: 'coffee not found' });
+    }
 };
 exports.editCoffeePage = editCoffeePage;
-const editCoffee = (req, res, next) => {
-    throw 'Not implemented';
+const editCoffee = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let updatedItem = req.body;
+    let [updated] = await coffee_1.Coffee.update(updatedItem, {
+        where: { coffeeId: itemId }
+    });
+    if (updated === 1) {
+        res.redirect('/coffee');
+    }
+    else {
+        res.render('error', { message: 'Coffee could not be updated' });
+    }
 };
 exports.editCoffee = editCoffee;
-const deleteCoffee = (req, res, next) => {
-    throw 'Not implemented';
+const deleteCoffee = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let deleted = await coffee_1.Coffee.destroy({
+        where: { coffeeId: itemId }
+    });
+    if (deleted) {
+        res.redirect('/coffee');
+    }
+    else {
+        res.status(404).render('error', { message: 'Cannot find item' });
+    }
 };
 exports.deleteCoffee = deleteCoffee;
--- src/controllers/coffeeController.ts
+++ src/controllers/coffeeController.ts
@@ -10,26 +10,69 @@ export const allCoffee: RequestHandler = async (req, res, next) => {
     res.render('all-coffee', { coffeeList });
 }
 
-export const oneCoffee: RequestHandler = (req, res, next) => {
-    throw 'Not implemented';
+export const oneCoffee: RequestHandler = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let coffeeItem: Coffee | null = await Coffee.findByPk(itemId);
+
+    if (coffeeItem) {
+        res.render('coffee-detail', { foundCoffee: coffeeItem });
+    }
+    else {
+        res.status(404).render('error', { message: 'coffee not found' });
+    }
 }
 
 export const addCoffeePage: RequestHandler = (req, res, next) => {
-    throw 'Not implemented';
+    res.render('add-coffee');
 }
 
-export const addCoffee: RequestHandler = (req, res, next) => {
-    throw 'Not implemented';
+export const addCoffee: RequestHandler = async (req, res, next) => {
+    let newCoffee: Coffee = req.body;
+    await Coffee.create(newCoffee);
+    res.redirect('/coffee');
 }
 
-export const editCoffeePage: RequestHandler = (req, res, next) => {
-    throw 'Not implemented';
+export const editCoffeePage: RequestHandler = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let coffeeItem: Coffee | null = await Coffee.findOne({
+        where: { coffeeId: itemId }
+    });
+
+    if (coffeeItem) {
+        res.render('edit-coffee', { foundCoffee: coffeeItem });
+    }
+    else {
+        res.status(404).render('error', { message: 'coffee not found' });
+    }
 }
 
-export const editCoffee: RequestHandler = (req, res, next) => {
-    throw 'Not implemented';
+export const editCoffee: RequestHandler = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+    let updatedItem: Coffee = req.body;
+
+    let [updated] = await Coffee.update(updatedItem, {
+        where: { coffeeId: itemId }
+    });
+
+    if (updated === 1) {
+        res.redirect('/coffee');
+    }
+    else {
+        res.render('error', { message: 'Coffee could not be updated' });
+    }
 }
 
-export const deleteCoffee: RequestHandler = (req, res, next) => {
-    throw 'Not implemented';
+export const deleteCoffee: RequestHandler = async (req, res, next) => {
+    let itemId = req.params.coffeeId;
+
+    let deleted = await Coffee.destroy({
+        where: { coffeeId: itemId }
+    });
+
+    if (deleted) {
+        res.redirect('/coffee')
+    }
+    else {
+        res.status(404).render('error', { message: 'Cannot find item' });
+    }
 }
